0899ea1d2d0d5ecf8f0a12b382cf987fb58e07be
kencyke <casek2703@gmail.com>
2019-08-13 18:58:49 +0900

    from tslint to typescript-eslint

---
+++ template/.eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+    "extends": [
+        "eslint:recommended",
+        "plugin:prettier/recommended",
+        "prettier/@typescript-eslint"
+    ],
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "sourceType": "module",
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "project": "./tsconfig.json"
+    },
+    "rules": {
+        "@typescript-eslint/adjacent-overload-signatures": "error",
+        "@typescript-eslint/array-type": "error",
+        "@typescript-eslint/ban-types": "error",
+        "camelcase": "off",
+        "@typescript-eslint/camelcase": "error",
+        "@typescript-eslint/class-name-casing": "error",
+        "@typescript-eslint/explicit-function-return-type": "warn",
+        "@typescript-eslint/explicit-member-accessibility": "error",
+        "no-unused-vars": "off",
+        "@typescript-eslint/no-unused-vars": "error",
+        "@typescript-eslint/no-use-before-define": "error",
+        "@typescript-eslint/no-var-requires": "error"
+    }
+}
---
+++ template/prettier.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    bracketSpacing: true,
+    printWidth: 120,
+    tabWidth: 4,
+    semi: false
+}
