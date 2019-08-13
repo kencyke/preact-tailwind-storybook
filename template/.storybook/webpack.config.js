3fb82e2f776e9d1f03c5c1512b35150a81e54f74
kencyke <casek2703@gmail.com>
2019-08-13 19:21:17 +0900

    minimize

--- template/.storybook/webpack.config.js
+++ template/.storybook/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = ({ config }) => {
     },
     {
       test: /\.s?css$/,
-      loaders: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      loaders: ["style-loader", "postcss-loader", "sass-loader"],
       include: path.resolve(__dirname, "../")
     }
   );
